7da04459e6c41550296a052e47c18539fd4cd160
David Lieb <d.lieb@gmx.de>
2016-04-24 12:04:55 +0200

    FAIL

---
+++ RBTV_GameJam/Assets/Scripts/CharacterSeleciton/Player.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+[Serializable]
+public class Player
+{
+
+    private PlayerEnum playerNumber;
+    private GameObject chosenCharacter;
+    private InputEnum controller;
+    private CharacterAvatar lockedAvatar;
+    private GameObject selectFrame;
+
+    public Player()
+    {
+        playerNumber = PlayerEnum.Player1;
+        controller = InputEnum.Joy1;
+        chosenCharacter = null;
+    }
+
+    public void setPlayer(PlayerEnum player)
+    {
+        this.playerNumber = player;
+    }
+
+    public CharacterAvatar GetLockedAvatar()
+    {
+        return this.lockedAvatar;
+    }
+
+    public void SetLockedAvatar(CharacterAvatar x)
+    {
+        this.lockedAvatar = x;
+    }
+    public PlayerEnum getPlayer()
+    {
+        return this.playerNumber;
+    }
+
+    public void setGameCharacter(GameObject character)
+    {
+        this.chosenCharacter = character;
+    }
+
+    public GameObject getGameCharacter()
+    {
+        return this.chosenCharacter;
+    }
+    public void setController(InputEnum input)
+    {
+        this.controller = input;
+    }
+
+    public InputEnum getJoyPad()
+    {
+        return this.controller;
+    }
+
+    public GameObject GetFrame()
+    {
+        return this.selectFrame;
+    }
+
+    public void SetSelectFrame(GameObject x)
+    {
+        selectFrame = x;
+    }
+}
---
+++ RBTV_GameJam/Assets/Scripts/FaceAnimation.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceAnimation : MonoBehaviour {
+
+
+    public PlatformerMotor2D _motor;
+    public Animator _faceAnimator;
+    public Animator _bodyAnimator;
+    private bool _isJumping;
+    private bool _currentFacingLeft;
+    public GameObject visualChild;
+
+    // Use this for initialization
+    void Start()
+    {
+        //_motor = GetComponent<PlatformerMotor2D>();
+        _faceAnimator.Play("HeadIdle");
+        _bodyAnimator.Play("BodyIdle");
+        _motor.onJump += SetCurrentFacingLeft;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_motor.motorState == PlatformerMotor2D.MotorState.Jumping ||
+            _isJumping &&
+                (_motor.motorState == PlatformerMotor2D.MotorState.Falling ||
+                             _motor.motorState == PlatformerMotor2D.MotorState.FallingFast))
+        {
+            _isJumping = true;
+            _faceAnimator.Play("HeadJump");
+            _bodyAnimator.Play("BodyJump");
+
+            if (_motor.velocity.x <= -0.1f)
+            {
+                _currentFacingLeft = true;
+            }
+            else if (_motor.velocity.x >= 0.1f)
+            {
+                _currentFacingLeft = false;
+            }
+
+        }
+        else
+        {
+            _isJumping = false;
+
+            if (_motor.motorState == PlatformerMotor2D.MotorState.Falling ||
+                             _motor.motorState == PlatformerMotor2D.MotorState.FallingFast)
+            {
+                //_faceAnimator.Play("HeadDash");
+                _bodyAnimator.Play("BodyDash");
+            }
+            else if (_motor.motorState == PlatformerMotor2D.MotorState.WallSliding ||
+                     _motor.motorState == PlatformerMotor2D.MotorState.WallSticking)
+            {
+                _faceAnimator.Play("HeadIdle");
+                _bodyAnimator.Play("BodyClimb");
+            }
+            else if (_motor.motorState == PlatformerMotor2D.MotorState.OnCorner)
+            {
+                _faceAnimator.Play("HeadIdle");
+                _bodyAnimator.Play("BodyClimb");
+            }
+            else if (_motor.motorState == PlatformerMotor2D.MotorState.Slipping)
+            {
+                _faceAnimator.Play("HeadIdle");
+                _bodyAnimator.Play("BodyClimb");
+            }
+            else if (_motor.motorState == PlatformerMotor2D.MotorState.Dashing)
+            {
+                _faceAnimator.Play("HeadRun");
+                _bodyAnimator.Play("BodyDash");
+            }
+            else
+            {
+                if (_motor.velocity.sqrMagnitude >= 0.1f * 0.1f)
+                {
+                    _bodyAnimator.Play("BodyRun");
+                    _faceAnimator.Play("HeadRun");
+
+                }
+                else
+                {
+                    _faceAnimator.Play("HeadIdle");
+                    _bodyAnimator.Play("BodyIdle");
+                }
+            }
+        }
+
+        // Facing
+        float valueCheck = _motor.normalizedXMovement;
+
+        if (_motor.motorState == PlatformerMotor2D.MotorState.Slipping ||
+            _motor.motorState == PlatformerMotor2D.MotorState.Dashing ||
+            _motor.motorState == PlatformerMotor2D.MotorState.Jumping)
+        {
+            valueCheck = _motor.velocity.x;
+        }
+
+        if (Mathf.Abs(valueCheck) >= 0.1f)
+        {
+            Vector3 newScale = visualChild.transform.localScale;
+            newScale.x = Mathf.Abs(newScale.x) * ((valueCheck > 0) ? 1.0f : -1.0f);
+            visualChild.transform.localScale = newScale;
+        }
+    }
+
+    private void SetCurrentFacingLeft()
+    {
+        _currentFacingLeft = _motor.facingLeft;
+    }
+}
+
+
